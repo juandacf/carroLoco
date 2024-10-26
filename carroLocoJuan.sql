f7ed87c953d38410eef752873fcb12acad83a079
JuanCaballero <juandacf@gmail.com>
2024-10-26 16:12:45 -0500

    final

--- carroLocoJuan.sql
+++ carroLocoJuan.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS ciudades
     nom_ciudad VARCHAR NOT NULL,
     val_latitud VARCHAR NOT NULL, 
     VAL_longitud SMALLINT NOT NULL,
-    PRIMARY KEY(id_ciudad),
+    PRIMARY KEY(id_ciudad)
 );
 CREATE TABLE IF NOT EXISTS tipoDocumento
 (
@@ -46,6 +46,20 @@ CREATE TABLE IF NOT EXISTS menus
     nom_prog VARCHAR NOT NULL,
     PRIMARY KEY(id_menu)
 );
+CREATE TABLE IF NOT EXISTS paises
+(
+    id_pais SMALLINT NOT NULL,
+    nom_pais VARCHAR NOT NULL,
+    PRIMARY KEY(id_pais)
+);
+CREATE TABLE IF NOT EXISTS marcas 
+(
+    id_marca SMALLINT NOT NULL,
+    nom_marca VARCHAR NOT NULL,
+    id_pais SMALLINT NOT NULL,
+    PRIMARY KEY(id_marca),
+    FOREIGN KEY(id_pais) REFERENCES paises(id_pais)
+);
 CREATE TABLE IF NOT EXISTS bancos 
 (
     id_banco SMALLINT NOT NULL,
@@ -53,22 +67,14 @@ CREATE TABLE IF NOT EXISTS bancos
     dir_banco VARCHAR NOT NULL,
     PRIMARY KEY(id_banco)
 );
-CREATE TABLE IF NOT EXISTS paises
-(
-    id_pais SMALLINT NOT NULL,
-    nom_pais VARCHAR NOT NULL,
-    PRIMARY KEY(id_pais)
-);
 CREATE TABLE IF NOT EXISTS usuarios 
 (
     id_usuario SMALLINT NOT NULL,
-    pass_usuario PASSWORD NOT NULL,
+    pass_usuario VARCHAR NOT NULL,
     mail_user VARCHAR NOT NULL,
-    tel_user INTEGER NOT NULL
+    tel_user INTEGER NOT NULL,
     PRIMARY KEY(id_usuario)
 );
-
-
 CREATE TABLE IF NOT EXISTS clientes
 (
     id_cliente INTEGER NOT NULL,
@@ -79,6 +85,10 @@ CREATE TABLE IF NOT EXISTS clientes
     num_hijos SMALLINT NOT NULL,
     direccion_cliente VARCHAR NOT NULL,
     id_usuario SMALLINT NOT NULL,
+    id_ciudad INTEGER NOT NULL,
+    id_tipoDocumento SMALLINT NOT NULL,
+    id_ocupacion SMALLINT NOT NULL,
+    id_profesion SMALLINT NOT NULL,
     PRIMARY KEY(id_cliente),
     FOREIGN KEY(id_ciudad)  REFERENCES ciudades(id_ciudad),
     FOREIGN KEY(id_tipoDocumento) REFERENCES tipoDocumento(id_tipoDocumento),
@@ -90,7 +100,7 @@ CREATE TABLE IF NOT EXISTS vehiculos
 (
     id_placa VARCHAR NOT NULL,
     num_motor VARCHAR NOT NULL,
-    var_modelo SMALLINT CHECK(val_modelo <= 2025),
+    var_modelo SMALLINT NOT NULL,
     var_capacidad SMALLINT NOT NULL,
     var_cilindraje SMALLINT NOT NULL,
     num_pasajeros SMALLINT NOT NULL,
@@ -101,7 +111,9 @@ CREATE TABLE IF NOT EXISTS vehiculos
     fec_importacion DATE NOT NULL,
     fec_matricula DATE NOT NULL,
     id_vendedor INTEGER NOT NULL,
+    id_marca SMALLINT NOT NULL,
     PRIMARY KEY(id_placa),
+    FOREIGN KEY(id_marca) REFERENCES marcas(id_marca)
 );
 CREATE TABLE IF NOT EXISTS vendedores
 (
@@ -126,11 +138,11 @@ CREATE TABLE IF NOT EXISTS vendedores
     valorComisiones INTEGER NOT NULL,
     porcentajeComision  SMALLINT NOT NULL,
     metaVentas INTEGER NOT NULL,
-    numHijos SMALLINT NOT NULL,
     id_estadoCivil SMALLINT NOT NULL,
-
-    PRIMARY KEY(id_vendedor)
-    FOREIGN KEY(id_estadoCivil) REFERENCES estadosCiviles(id_estadoCivil)
+    id_usuario SMALLINT NOT NULL,
+    PRIMARY KEY(id_vendedor),
+    FOREIGN KEY(id_estadoCivil) REFERENCES estadosCiviles(id_estadoCivil),
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario)
 );
 CREATE TABLE IF NOT EXISTS proveedores
 (
@@ -143,7 +155,7 @@ CREATE TABLE IF NOT EXISTS proveedores
     direccion VARCHAR NOT NULL,
     id_tipoDocumento VARCHAR NOT NULL,
     id_usuario SMALLINT NOT NULL,
-    PRIMARY KEY(id_proveedor)
+    PRIMARY KEY(id_proveedor),
     FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario)
 
 );
@@ -161,7 +173,7 @@ CREATE TABLE IF NOT EXISTS vehiculosVendedor
     id_vendedor INTEGER NOT NULL,
     PRIMARY KEY(id_placa, id_vendedor),
     FOREIGN KEY(id_placa) REFERENCES vehiculos(id_placa),
-    FOREIGN KEY(id_vendedor) REFERENCES vendedores(id_vendedores)
+    FOREIGN KEY(id_vendedor) REFERENCES vendedores(id_vendedor)
 );
 CREATE TABLE IF NOT EXISTS bancosProveedor
 (
